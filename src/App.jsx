f17d05ea3e7e67ceaafe32d47576e66cddb4f484
bonifaceben <bonifaceb313@gmail.com>
2025-06-10 09:08:43 -0700

    Fix: corrected case-sensitive import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FootBall from './pages/footBall';
-import BasketBall from './pages/basketBall';
+import BasketBall from './pages/BasketBall';
 import Header from './components/Header'
 import Subnav from './components/Subnav';
 
